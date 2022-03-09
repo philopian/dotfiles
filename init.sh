3db9f809ee6907b5ce1622f09b739fa337b480b1
Phil Willis <philopian@gmail.com>
2022-03-09 00:47:00 -0800

    init

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export $(grep -v '^#' .colors | xargs)
+
+
+printf "\n${WHITE}==============================================================================
+SETTING UP YOUR COMPUTER!
+==============================================================================${NORMAL}\n"
+
+# Install XCode Command Line Tools
+printf "\n${YELLOW}Installing XCode Command Line Tools...${NORMAL}\n"
+xcode-select --install
+
+# Install Homebrew and its packages
+printf "\n${YELLOW}Installing home-brew...${NORMAL}\n"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+zsh ./.brew
+zsh ./.shell
+zsh ./.nodejs
+zsh ./.vscode
+zsh ./.open-dl-links
+
+printf "\n${BLUE}All DONE!${NORMAL}\n"
