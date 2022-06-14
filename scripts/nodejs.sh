dad5afc2931582e273fd3344c9c9ddd1bdaabd8e
Phil Willis <philopian@gmail.com>
2022-06-14 10:57:28 -0700

    update nodejs

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -10,14 +10,15 @@ mkdir ~/.nvm
 # Install NVM
 printf "\n${YELLOW}Installing NVM...${NORMAL}\n"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-
 source ~/.zshrc
 
-nvm install node --lts
-nvm alias default node
 nvm install --lts=Erbium # node 12
 nvm install --lts=Fermium # node 14
+nvm install --lts=Gallium # node 16
+nvm alias default lts/gallium
 
-npm i -g live-server 
+npm i -g live-server yarn 
+
+printf "\n${GREEN}Nodejs installed${NORMAL}\n"
 
 source ~/.zshrc
